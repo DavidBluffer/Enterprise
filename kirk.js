a9ab76e2c2b867312a09f5866e40e267719f02c8
David Zurbaran <zurbaranone@outlook.com>
2020-07-23 16:20:02 -0500

    Initial commit

---
+++ kirk.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can I Do?</h2>
+
+<p id="demo">I can salute a Beauty Queen.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello Miss Universe!"'>Click Me!</button>
+
+</body>
+</html>
